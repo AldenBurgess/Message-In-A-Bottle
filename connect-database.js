17eb8b36cb87dddbcce24c2fd1c6b63f29a0b2d5
AnandaMontoly <amontoly@smith.edu>
2020-12-19 02:55:51 -0500

    documentation mi amigo. also cleaning up mess

--- connect-database.js
+++ connect-database.js
@@ -1,47 +1,51 @@
 const { Client } = require("cassandra-driver");
+//ofc express
 const express = require('express');
 const app = express();
+//need this in order to make req work right
 var bodyParser = require('body-parser');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 var port = process.env.PORT || 8080;        // set our port
-var router = express.Router();
+var router = express.Router();  //router as usual
 
+//connecting to the server and all that stuff you need to do
 const client = new Client({
   cloud: {
+    //ssl certs in directory - make sure you upload to google cloud!!!
     secureConnectBundle: "./secure-connect-bottledmessage.zip",
   },
+  //ye
   credentials: { username: "passthelad", password: "CoolKid56" },
 });
+///https://stackoverflow.com/questions/46378266/how-to-make-nodejs-cassandra-driver-work-in-lambda
+//for some reason you only connect once and hten you don't disconnect? weird
 client.connect();
+
+//default my house for testing purposes
 async function query(placeName = "anandaHouse") {
-
-
-  // Execute a query
   const rs = await client.execute("SELECT messageText, postTime FROM messages.messagedb WHERE placeName = '"+placeName+"';");
-  for (row of rs["rows"]){
-    console.log(row["messagetext"]+" posted on "+(row["posttime"].getMonth()+1)+"/"+row["posttime"].getDate());
-  }
   return rs["rows"]
 }
 
 async function insertInto(messageText, postTime, placeName){
-
+  //alright boys, time to build anti sql injection technology
+  //https://stackoverflow.com/questions/31822891/how-to-build-dynamic-query-by-binding-parameters-in-node-js-sql implement in the morning
   await client.execute("INSERT INTO messages.messagedb (messageText, postTime, placeName) VALUES ('"+messageText+"', '"+postTime+"', '"+placeName+"');");
 }
 
-//get stuff for user. have conversation with alden about how to insert information
+//get stuff for user. have conversation with alden about how to get place name
 router.get('/', async function(req, res) {
-    const queryInfo = await query();
+    const queryInfo = await query(req.body.placeName);
     await res.json(queryInfo);
 });
-
-//insertInto("My name is Ananda and I like sandwiches", "2020-12-18 1:33", "anandaHouse")
+//
+router.post('/postStuff', async function(req, res){
+  insertInto(req.body.messageText, req.body.postTime, req.body.placeName);
+});
+///make it use /api whenever using router routes
 app.use('/api', router);
 app.listen(port);
-insertInto("hi", '2020-12-18 12:33', "anandaHouse")
 //getting moving. https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4
 console.log('working on port ' + port);
