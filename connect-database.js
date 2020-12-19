4ec34e59a291eb697f39d9a4aa96cd6cd1a21480
AnandaMontoly <amontoly@smith.edu>
2020-12-19 01:53:15 -0500

    building the api server

--- connect-database.js
+++ connect-database.js
@@ -10,37 +10,38 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8080;        // set our port
 var router = express.Router();
 
+const client = new Client({
+  cloud: {
+    secureConnectBundle: "./secure-connect-bottledmessage.zip",
+  },
+  credentials: { username: "passthelad", password: "CoolKid56" },
+});
+client.connect();
 async function query(placeName = "anandaHouse") {
-  const client = new Client({
-    cloud: {
-      secureConnectBundle: "./secure-connect-bottledmessage.zip",
-    },
-    credentials: { username: "passthelad", password: "CoolKid56" },
-  });
 
-  await client.connect();
 
   // Execute a query
   const rs = await client.execute("SELECT messageText, postTime FROM messages.messagedb WHERE placeName = '"+placeName+"';");
-  console.log(rs["rows"])
   for (row of rs["rows"]){
     console.log(row["messagetext"]+" posted on "+(row["posttime"].getMonth()+1)+"/"+row["posttime"].getDate());
   }
-  await client.shutdown();
+  return rs["rows"]
 }
 
 async function insertInto(messageText, postTime, placeName){
-  const client = new Client({
-    cloud: {
-      secureConnectBundle: "./secure-connect-bottledmessage.zip",
-    },
-    credentials: { username: "passthelad", password: "CoolKid56" },
-  });
 
-  await client.connect();
-  await client.execute("INSERT INTO messagedb (messageText, postTime, placeName) VALUES ('"+messageText+"', '"+postTime+"', '"+placeName+"');");
-  await client.shutdown();
+  await client.execute("INSERT INTO messages.messagedb (messageText, postTime, placeName) VALUES ('"+messageText+"', '"+postTime+"', '"+placeName+"');");
 }
 
-// Run the async function
-query();
+//get stuff for user. have conversation with alden about how to insert information
+router.get('/', async function(req, res) {
+    const queryInfo = await query();
+    await res.json(queryInfo);
+});
+
+//insertInto("My name is Ananda and I like sandwiches", "2020-12-18 1:33", "anandaHouse")
+app.use('/api', router);
+app.listen(port);
+insertInto("hi", '2020-12-18 12:33', "anandaHouse")
+//getting moving. https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4
+console.log('working on port ' + port);
