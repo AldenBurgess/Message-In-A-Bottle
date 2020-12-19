05adfc97131ce2a291c589a0339ba76f68155b93
AnandaMontoly <amontoly@smith.edu>
2020-12-19 12:56:29 -0500

    testing api

---
+++ testapi/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+//need this in order to make req work right
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var port = process.env.PORT || 5000;        // set our port
+var router = express.Router();  //router as usual
+
+
+
+
+app.get('/', function (req, res) {
+  res.send('get request to the homepage')
+})
+
+app.listen(port);
