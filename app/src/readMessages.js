9ea79492f10f1e23ce6372805cb81a61281d7e18
AnandaMontoly <amontoly@smith.edu>
2020-12-20 03:24:13 -0500

    read message object for user to see local messages

---
+++ app/src/readMessages.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Form from 'react-bootstrap/Form';
+import Radar from 'radar-sdk-js';
+import Button from 'react-bootstrap/Button';
+var Cassandra = require('./useAPI.js');
