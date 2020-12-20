d5d49926cb3facb80541c3d907aa6d35546ee51d
AnandaMontoly <amontoly@smith.edu>
2020-12-20 04:13:53 -0500

    adding in stuff

--- app/src/index.js
+++ app/src/index.js
@@ -4,9 +4,101 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class HomeText extends React.Component{
+  constructor (){
+    super();
+  }
+  render(){
+    return (
+      <h3>Home</h3>
+    )
+  }
+}
+
+class AboutText extends React.Component{
+  constructor (){
+    super();
+  }
+  render(){
+    return (
+      <div>
+        <h1>About</h1>
+        <p>Notes in Space is an interactive web experience wherein users can find and leave notes in AR for people in their
+        vicinity. </p>
+      </div>
+    )
+  }
+}
+
+class Tabs extends React.Component {
+  constructor () {
+    super();
+  }
+  render() {
+    return (
+    <div id="encloses">
+      <Tab.Container id="pagecontent" defaultActiveKey="home">
+        <Row style={{marginLeft:"1px", marginRight:"1px"}}>
+          <Col sm={3}>
+            <Nav variant="pills" className="flex-column">
+              <Nav.Item>
+                <Nav.Link eventKey="home">Home</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="about">About</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={9}>
+            <Tab.Content>
+              <Tab.Pane eventKey="home">
+                <HomeText />
+              </Tab.Pane>
+              <Tab.Pane eventKey="about">
+                <AboutText />
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+    </div>
+  )
+  }
+}
+
+class Header extends React.Component {
+  constructor() {
+    super();
+  }
+  render () {
+    return (
+      <div>
+        <Jumbotron fluid style={{backgroundColor:"#326128", paddingTop:"25px", paddingBottom:"25px", color:"#fffffc"}}>
+          <Container>
+            <h1>Notes in Space</h1>
+          </Container>
+        </Jumbotron>
+      </div>
+    )
+  }
+}
+
+class Page extends React.Component {
+  constructor () {
+    super();
+  }
+  render () {
+    return (<div style={{backgroundColor:"#fffffc"}}>
+        <Header />
+        <Tabs />
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Page />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- app/src/readMessages.js
+++ app/src/readMessages.js
@@ -4,3 +4,43 @@ import Form from 'react-bootstrap/Form';
 import Radar from 'radar-sdk-js';
 import Button from 'react-bootstrap/Button';
 var Cassandra = require('./useAPI.js');
+
+class ReadMessage extends React.Component{
+  constructor (props) {
+    super(props);
+    this.state = {Messages:[]};
+  }
+  getData(){
+    Radar.initialize("prj_test_pk_d65e98b983fe32401d19717c4c222098ee35251a ");
+    Radar.searchGeofences({
+      radius: 1000,
+      tags: ['userAddress','college'],
+      limit: 10
+    }, function(err, result) {
+      if (!err) {
+        if (result.geofences.length>0){
+          //what the fuck why am I getting goddamn security errors what the fuck
+          //console.log(Cassandra.getRequest(result.geofences[0]["externalId"]));
+          this.requestData('neighborHouse')
+        }else {
+          return ["Nothing to display"]
+        }
+      }
+    });
+  }
+  requestData(placeName) {
+    // Simple GET request using fetch
+    fetch('cassandra-api-dot-bottlemessage-299107.uc.r.appspot.com/api/query/'+placeName)
+        .then(response => response.json())
+        .then(data => this.setState({ Messages: data.total }));
+      }
+  render(){
+    this.getData();
+    return(
+      <p>{this.state.Messages}</p>
+    )
+  }
+}
+
+
+  export default ReadMessage;
